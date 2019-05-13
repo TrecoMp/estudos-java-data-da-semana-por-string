8fe75f0173f7a6480d790280274123a3b5071793
Marcos Paulo Fernandes <40579498+TrecoMp@users.noreply.github.com>
2019-05-13 14:39:19 -0300

    Add files via upload

---
+++ UsoCalendar.java
@@ -0,0 +1,22 @@
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class UsoCalendar {
+	    public static void main(String[] args) {
+	        String data = "13/05/2019";
+	        
+	        GregorianCalendar gc = new GregorianCalendar();
+	        
+	        try {
+				gc.setTime(new SimpleDateFormat("dd/MM/yyyy").parse(data));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+	        System.out.println("Data Agora: "+gc.get(Calendar.DAY_OF_WEEK));
+	}
+}
